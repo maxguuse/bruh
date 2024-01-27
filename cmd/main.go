b39f26f626dbac82fbcee8448398f00a3572eeba
maxguuse <max.pink.goose@gmail.com>
2024-01-27 16:26:42 +0300

    feat: add charmbracelet/huh and charmbracelet/bubbletea TUI libraries + created base form

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/charmbracelet/huh"
+)
+
+type ModuleType int
+
+const (
+	App ModuleType = iota
+	Lib
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var moduleType ModuleType
+	var moduleName string
+
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[ModuleType]().
+				Title("Module Type").
+				Options(
+					huh.NewOption("Application", App),
+					huh.NewOption("Library", Lib),
+				).
+				Value(&moduleType),
+			huh.NewInput().
+				Title("Module Name").
+				CharLimit(20).
+				Value(&moduleName),
+		),
+	).Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch moduleType {
+	case App:
+		log.Println("App: ", moduleName)
+	case Lib:
+		log.Println("Lib: ", moduleName)
+	}
 }
