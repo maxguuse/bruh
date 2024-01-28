808d017f2b10435ddb775527e463b518e319b131
maxguuse <max.pink.goose@gmail.com>
2024-01-28 17:12:21 +0300

    refactor: changed subcommand choosing from switch to map

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,19 +1,26 @@
 package app
 
 import (
+	"log"
+
 	"github.com/maxguuse/bruh/internal/cmds/create_module"
 	"github.com/maxguuse/bruh/internal/cmds/init_project"
 	"github.com/maxguuse/bruh/internal/forms"
 	"github.com/maxguuse/bruh/internal/types"
 )
 
+var subcommands = map[types.SubcommandType]func(){
+	types.InitProject:  init_project.Cmd,
+	types.CreateModule: create_module.Cmd,
+}
+
 func Run() {
 	subcommand := forms.NewWelcome().Run()
 
-	switch subcommand {
-	case types.InitProject:
-		init_project.Cmd()
-	case types.CreateModule:
-		create_module.Cmd()
+	handler, ok := subcommands[subcommand]
+	if !ok {
+		log.Fatal("Invalid subcommand")
 	}
+
+	handler()
 }
